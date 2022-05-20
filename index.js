ed6309c6c9e2633f72a6211b4ea209023465e7aa
Kamelia AIT ISSAD <kaitissad@PDLA029190F2C39.elite-telecoms.local>
2022-05-20 10:47:04 +0200

    Création de l'interface index.html et modification de index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const puppeteer = require("puppeteer");
 
+
+function Rechercher(){
+  if(document.getElementById('NbPages').value != null){
+    nombre_pages=document.getElementById('NbPages').value;
+  }
+  if(document.getElementById('Recherche').value != null){
+    recherche=document.getElementById('Recherche').value;
+  }
+}
+
 const f = async () => {
   const browser = await puppeteer.launch({ headless: false }); // par default --> true
   const page = await browser.newPage();
